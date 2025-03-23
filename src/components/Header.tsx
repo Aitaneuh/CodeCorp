5bcc966d1bfbce4f5b1015a577b73740930dbc2a
Aitaneuh <ethanb9407@gmail.com>
2025-03-23 22:51:17 +0100

    fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import { FaGithub } from "react-icons/fa";
 
 const Header: React.FC = () => {
+
     return (
         <header className="header">
+            <a href="https://github.com/Aitaneuh/codecorp" target="_blank" rel="noopener noreferrer" className="github-link">
+                <FaGithub className="github-icon" /> CodeCorp 
+            </a>
             <h1 className="title">CodeCorp</h1>
             <a href="https://github.com/Aitaneuh" target="_blank" rel="noopener noreferrer" className="github-link">
                 Aitaneuh <FaGithub className="github-icon" />
